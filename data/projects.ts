490c47c8cbd5d56f7bd5633bc238fbd64d16bdf1
Vincent <mcken.vince@gmail.com>
2024-02-14 10:51:04 -0700

    Add projects, move to separate file and add to paths in tsconfig

---
+++ data/projects.ts
@@ -0,0 +1,59 @@
+export const myProjects = [
+  {
+    title: "MySudoku",
+    description: "A Sudoku puzzle generator.",
+    techStack: ["React", "Typescript"],
+    github: "https://github.com/mcken-vince/mysudoku",
+  },
+  {
+    title: "CrewLauncher Rebuild",
+    description:
+      "A rebuild of CrewLauncher, learning from the rushed build of the original.",
+    techStack: [
+      "Typescript",
+      "React",
+      "MongoDB",
+      "Axios",
+      "SCSS",
+      "react-bootstrap",
+    ],
+    github: "https://github.com/mcken-vince/CrewLaunch",
+  },
+  {
+    title: "CrewLauncher",
+    note: "Group project with two-week deadline",
+    description:
+      "A dual-sided application designed for a company dispatcher and crews to track contracts, services and jobs.",
+    techStack: [
+      "React",
+      "Express",
+      "PostgreSQL",
+      "Socket.io",
+      "Axios",
+      "Knex",
+      "SCSS",
+      "MUI",
+    ],
+    github: "https://github.com/mcken-vince/CrewLauncher",
+  },
+  {
+    title: "Scheduler",
+    description: "A scheduler app - for managing interview appiontments.",
+    techStack: ["Node", "Express", "PostgreSQL", "React", "HTML", "SCSS"],
+    github: "https://github.com/mcken-vince/scheduler",
+  },
+  {
+    title: "SmartList",
+    description:
+      "Auto-categorize user inputs as a movie, book, food, or a different product.",
+    techStack: ["Node", "Express", "PostgreSQL", "JQuery", "Ajax", "HTML", "CSS"],
+    github: "https://github.com/mcken-vince/SmartList",
+  },
+  {
+    title: "Tweeter",
+    description:
+      "A single-page Twitter clone where a user can compose and view tweets.",
+    techStack: ["Node", "Express", "MongoDB", "JQuery, AJAX", "HTML", "CSS"],
+    github: "https://github.com/mcken-vince/tweeter",
+  },
+];
