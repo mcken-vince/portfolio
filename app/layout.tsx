067d9e045de2670b46a608b24f2e949330671099
Vincent <mcken.vince@gmail.com>
2024-02-14 10:53:18 -0700

    Export components and update imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Navigation } from "@components/Navigation";
-import { Footer } from "@components/Footer";
+import { Navigation, Footer } from "@components";
 
 const inter = Inter({ subsets: ["latin"] });
 
---
+++ components/index.ts
@@ -0,0 +1,12 @@
+// Layout
+export * from './Navigation';
+export * from './Footer';
+
+// Pages
+export * from './Main';
+export * from './Project';
+
+// Links
+export * from './Links/GithubLink'
+export * from './Links/LinkedinLink'
+export * from './Links/NavLink'
